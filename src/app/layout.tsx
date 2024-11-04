eb21afb5b8a08f2ae8c54db1b84b143ba29c5012
Karthikeya <veruturikarthikeya@gmail.com>
2024-11-04 21:19:07 +0530

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,50 @@ import Providers from "@/context/Providers";
 import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
-  title: "Photolic - AI photo generator",
-  description: "",
+  title: "Photolic - AI Photo Generator",
+  description:
+    "Create stunning images from text with Photolic, the AI-powered photo generator. Transform your ideas into visuals effortlessly!",
+  keywords: [
+    "AI photo generator",
+    "text to image",
+    "image creation",
+    "AI art generator",
+    "generate images from text",
+    "creative tools",
+    "digital art",
+  ],
+  authors: { url: "https://itskv.me", name: "Karthikeya Veruturi" },
+  viewport: "width=device-width, initial-scale=1.0",
+  robots: "index, follow",
+  openGraph: {
+    title: "Photolic - AI Photo Generator",
+    description:
+      "Generate unique images from text prompts using Photolic's advanced AI technology. Ideal for artists, marketers, and creative minds.",
+    url: "https://photolic.itskv.me",
+    siteName: "Photolic",
+    images: [
+      {
+        url: "https://photolic.itskv.me/og1.png",
+        width: 1900,
+        height: 970,
+        alt: "Photolic image generation interface dashboard",
+      },
+      {
+        url: "https://photolic.itskv.me/og2.png",
+        width: 1900,
+        height: 970,
+        alt: "Image generation example using Photolic",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@karthikeyaspace",
+    title: "Photolic - AI Photo Generator",
+    description:
+      "Turn your text into stunning images with Photolic's AI technology.",
+    images: "https://photolic.itskv.me/og2.png",
+  },
 };
 
 export default function RootLayout({
