3257d929938f54aac39665e1cee8821f4b1f04d5
Karthikeya <veruturikarthikeya@gmail.com>
2024-11-26 19:09:05 +0530

    26/11: never forget, never forgive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import { getServerSessionAuth } from "@/lib/auth";
-import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default function Home() {
-  const session = getServerSessionAuth()
-  if(!session) return redirect("/login")
-  return redirect("/create");
+  return (
+    <div className="w-full h-[calc(100vh-100px)] flex flex-col justify-center items-center">
+      <h1>Will make a landing page after getting funded</h1>
+      <Link href={"/login"} className="underline text-blue-600">
+        login
+      </Link>
+    </div>
+  );
 }
